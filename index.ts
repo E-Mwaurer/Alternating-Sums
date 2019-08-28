ba3c873b269a3a5695cd9668bf5bfb217c7205fa
EdwardMwaura <edduemwaurer@outlook.com>
2019-08-28 16:18:10 +0300

    2 files modified

--- index.ts
+++ index.ts
@@ -3,4 +3,10 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Alternating Sums</h1>`;
+
+function alternatingSums(a: number[]): number[] {
+
+}
+
+console.log(alternatingSums([50, 60, 60, 45, 70]))
